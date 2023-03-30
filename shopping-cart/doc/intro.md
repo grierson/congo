cb0b46f2a0ee66a75aefe5842060effe5568bb45
Kyle Grierson <griersonky@gmail.com>
2023-03-30 22:45:51 +0100

    init

---
+++ shopping-cart/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to congo/shopping-cart
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ shopping-cart/test/congo/shopping_cart_test.clj
@@ -0,0 +1,7 @@
+(ns congo.shopping-cart-test
+  (:require [clojure.test :refer :all]
+            [congo.shopping-cart :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

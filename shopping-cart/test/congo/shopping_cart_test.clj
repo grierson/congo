b4da0e761ae448a51b738984ef1d3b962e13bb24
Kyle Grierson <griersonky@gmail.com>
2023-03-31 21:58:19 +0100

    Redis

--- shopping-cart/test/congo/shopping_cart_test.clj
+++ shopping-cart/test/congo/shopping_cart_test.clj
@@ -1,7 +1,24 @@
 (ns congo.shopping-cart-test
   (:require [clojure.test :refer [deftest is testing]]
+            [freeport.core :refer [get-free-port!]]
             [congo.shopping-cart :refer [app]]
-            [jsonista.core :as j]))
+            [jsonista.core :as j]
+            [taoensso.carmine :as car :refer [wcar]])
+  (:import (com.github.fppt.jedismock RedisServer)))
+
+(def server (RedisServer/newRedisServer))
+(def redis-host (.getHost server))
+(def redis-port (.getBindPort server))
+(.start server)
+(def redis-url (str "redis://" redis-host ":" redis-port))
+
+(def my-wcar-opts
+  {:pool (car/connection-pool {})
+   :spec {:uri redis-url}})
+
+(wcar my-wcar-opts (car/ping))
+(wcar my-wcar-opts (car/set "foo" "bar"))
+(wcar my-wcar-opts (car/get "foo"))
 
 (deftest health-test
   (testing "Calling health returns 200"
@@ -16,7 +33,8 @@
       (testing "returns 200"
         (is (= 200 (:status request))))
       (testing "body"
-        (is (= {:user-id 42}
+        (is (= {:user-id 42
+                :items [{:name "t-shirt"}]}
                (-> request
                    :body
                    slurp
